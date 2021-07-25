d75faa21344655e92d241727384970d2495a14ae
German Baena <germanb66@gmail.com>
2021-07-25 15:59:06 -0500

    Se actualizo "iconCheckin y iconContact" en "scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,8 @@ addEventListener('DOMContentLoaded', () => {
             burguerButton = document.querySelector('.burguer-button'),
             burguerLine = document.querySelector('.burguer-button__line'),
             nav = document.getElementById('main-nav'),
+            iconCheckin = document.getElementById('checkin'),
+            iconContact = document.getElementById('contact'),
             mql = matchMedia('(min-width: 1024px)')
 
 
@@ -22,13 +24,14 @@ addEventListener('DOMContentLoaded', () => {
     // Función change icons.
     const changeIcons = () => {
         if (mql.matches) {
-            document.getElementById("checkin").src = "img/icons/checkin-desk.svg"
-            document.getElementById("contact").src = "img/icons/contact-desk.svg"
+            iconCheckin.src = "img/icons/checkin-desk.svg"
+            iconContact.src = "img/icons/contact-desk.svg"
         } else {
-            document.getElementById("checkin").src = "img/icons/checkin-mobl.svg"
-            document.getElementById("contact").src = "img/icons/contact-mobl.svg"
+            iconCheckin.src = "img/icons/checkin-mobl.svg"
+            iconContact.src = "img/icons/contact-mobl.svg"
         }
     }
+
     // Ejecución de la función change icons.
     changeIcons()
 
